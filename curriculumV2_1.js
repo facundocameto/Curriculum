115fda07a5ccae35a30fe8570fa01e25bb4a3489
facundocameto <68877564+facundocameto@users.noreply.github.com>
2020-08-24 00:57:49 -0300

    slide

--- curriculumV2_1.js
+++ curriculumV2_1.js
@@ -1,4 +1,6 @@
 gmail_but = document.getElementById("gmail_but");
+var tch_hideable = document.getElementById("tch_hideable");
+var tch_hideable_2 = document.getElementById("tch_hideable_2");
 
 function copyMail()
 {
@@ -8,3 +10,23 @@ function copyMail()
     this.focus();
 };
 gmail_but.addEventListener("click", copyMail);
+
+function hideOrShow()
+{
+  var hideable = document.getElementById("hideable");
+  var hideable_2 = document.getElementById("hideable_2");
+  if (hideable.style.display == "none")
+  {
+    hideable.style.display = "flex";
+    hideable_2.style.display = "inline-grid";
+
+  }
+  else
+  {
+    hideable.style.display = "none";
+    hideable_2.style.display = "none";
+  }
+}
+tch_hideable.addEventListener("click", hideOrShow);
+tch_hideable_2.addEventListener("click", hideOrShow);
+document.addEventListener("DOMContentLoaded", hideOrShow);
